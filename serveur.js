33f9e6b1acaac975c6ad80fc2514ef6a7bdb0d05
Rulio974 <57720375+Rulio974@users.noreply.github.com>
2022-05-23 21:27:58 +0200

    2

--- serveur.js
+++ serveur.js
@@ -5,50 +5,50 @@ const fs = require('fs');
 var path = require('path');
 const session = require('express-session');
 var bodyParser = require('body-parser')
-const metadata = require('gcp-metadata');
-const {OAuth2Client} = require('google-auth-library');
+// const metadata = require('gcp-metadata');
+// const {OAuth2Client} = require('google-auth-library');
 
 
-const oAuth2Client = new OAuth2Client();
-let aud;
+// const oAuth2Client = new OAuth2Client();
+// let aud;
 
 var app = express();
 
-async function audience() {
-    if (!aud && (await metadata.isAvailable())) {
-      let project_number = await metadata.project('numeric-project-id');
-      let project_id = await metadata.project('project-id');
+// async function audience() {
+//     if (!aud && (await metadata.isAvailable())) {
+//       let project_number = await metadata.project('numeric-project-id');
+//       let project_id = await metadata.project('project-id');
   
-      aud = '/projects/' + project_number + '/apps/' + project_id;
-    }
+//       aud = '/projects/' + project_number + '/apps/' + project_id;
+//     }
   
-    return aud;
-  }
+//     return aud;
+//   }
 
-  async function validateAssertion(assertion) {
-    if (!assertion) {
-      return {};
-    }
+//   async function validateAssertion(assertion) {
+//     if (!assertion) {
+//       return {};
+//     }
   
-    // Check that the assertion's audience matches ours
-    const aud = await audience();
+//     // Check that the assertion's audience matches ours
+//     const aud = await audience();
   
-    // Fetch the current certificates and verify the signature on the assertion
-    const response = await oAuth2Client.getIapPublicKeys();
-    const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
-      assertion,
-      response.pubkeys,
-      aud,
-      ['https://cloud.google.com/iap']
-    );
-    const payload = ticket.getPayload();
+//     // Fetch the current certificates and verify the signature on the assertion
+//     const response = await oAuth2Client.getIapPublicKeys();
+//     const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
+//       assertion,
+//       response.pubkeys,
+//       aud,
+//       ['https://cloud.google.com/iap']
+//     );
+//     const payload = ticket.getPayload();
   
-    // Return the two relevant pieces of information
-    return {
-      email: payload.email,
-      sub: payload.sub,
-    };
-  }
+//     // Return the two relevant pieces of information
+//     return {
+//       email: payload.email,
+//       sub: payload.sub,
+//     };
+//   }
   
 
 app.use(session({
@@ -58,17 +58,17 @@ app.use(session({
     saveUninitialized: false,
 }));
 
-app.get('/', async (req, res) => {
-    const assertion = req.header('X-Goog-IAP-JWT-Assertion');
-    let email = 'None';
-    try {
-      const info = await validateAssertion(assertion);
-      email = info.email;
-    } catch (error) {
-      console.log(error);
-    }
-    res.status(200).send(`Hello ${email}`).end();
-  });
+// app.get('/', async (req, res) => {
+//     const assertion = req.header('X-Goog-IAP-JWT-Assertion');
+//     let email = 'None';
+//     try {
+//       const info = await validateAssertion(assertion);
+//       email = info.email;
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     res.status(200).send(`Hello ${email}`).end();
+//   });
   
 
 
